1c41fc52ec8656274cf2ad42392f8d868407edd9
Vadim Bond <wadimbond@mail.ru>
2020-05-23 22:28:32 +0200

    catalog responsive

--- src/js/_event.js
+++ src/js/_event.js
@@ -10,30 +10,45 @@ function handlerProducts(e) {
 }
 
 const links = document.querySelectorAll(".product-info__link");
-
 for (let i = 0; i < links.length; i++) {
   links[i].addEventListener("click", handlerProducts);
 }
 
 // handler for tabs
 function handlerTabs(e) {
-  // e.preventDefault();
-  console.log(e);
-  
   const contentName = e.target.dataset.tab;
-  console.log(contentName);
-  
-  // const contents = document.querySelectorAll(`.${contentName}`);
-  
-  // for (let i = 0; i < contents.length; i++) {
-  //   contents[i].classList.toggle("hidden");
-  // }
+  const contentNumber = contentName[contentName.length - 1];
+  const contents = document.querySelectorAll(".catalog-content");
+
+  if (!contents[contentNumber-1].matches(".hidden")) {
+    return;
+  } else {
+
+    tabs[contentNumber-1].classList.add("catalog-tabs__tab--active");
+    for (let i = 0; i < tabs.length; i++) {
+      if (i == contentNumber - 1) {
+        continue;
+      } else {
+        if (tabs[i].matches(".catalog-tabs__tab--active")) {
+          tabs[i].classList.remove("catalog-tabs__tab--active");
+        }
+      }
+    }
+        
+    contents[contentNumber-1].classList.remove("hidden");
+    for (let i = 0; i < contents.length; i++) {
+      if (i == contentNumber - 1) {
+        continue;
+      } else {
+        if (!contents[i].matches(".hidden")) {
+          contents[i].classList.add("hidden");
+        }
+      }
+    }
+  }
 }
 
 const tabs = document.querySelectorAll(".catalog-tabs__tab");
-console.log(tabs);
-
-
 for (let i = 0; i < tabs.length; i++) {
   tabs[i].addEventListener("click", handlerTabs);
 }
